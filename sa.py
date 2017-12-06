f38c12412b95515f17323196d716e4e071ea3186
Marcos Balatka <m.balatka5@gmail.com>
2017-12-06 00:21:14 -0200

    SA funcionando

--- sa.py
+++ sa.py
@@ -7,9 +7,12 @@ Marcos Balatka
 import os, random, math
 
 #parametros
-entrada = "uf20-01.cnf"
-t0 = 50#temperatura inicial
-tf = 0#temperatura final
+entrada = "uf250-01.cnf"
+t0 = 70#temperatura inicial
+tf = 0.01#temperatura final
+
+maxIt = 500000#quantidade maxima de iteracoes
+txDecT = 0.0001
 
 def clear():
     os.system("cls")
@@ -30,22 +33,22 @@ def readFile(entrada):#ler entrada
     return p, int(linhas[7].split()[2]), int(linhas[7].split()[3])
 
 def decTemperatura(t):#decaimento da temperatura
-    return t - 0.005
+    return t * (1 - txDecT)
     
-def energia(prob, resp):
+def energia(prob, resp, qtdC):
     e = 0
     for clausula in prob:
-        if clausula[0] < 0 and resp[-clausula[0] - 1] == 0 or clausula[0] >= 0 and resp[clausula[0] - 1] == 1:#prep 1
+        if clausula[0] < 0 and resp[(-clausula[0]) - 1] == 0 or clausula[0] >= 0 and resp[clausula[0] - 1] == 1:#prep 1
             p0 = 1
         else:
             p0 = 0
         
-        if clausula[1] < 0 and resp[-clausula[1] - 1] == 0 or clausula[1] >= 0 and resp[clausula[1] - 1] == 1:#prep 2
+        if clausula[1] < 0 and resp[(-clausula[1]) - 1] == 0 or clausula[1] >= 0 and resp[clausula[1] - 1] == 1:#prep 2
             p1 = 1
         else:
             p1 = 0
             
-        if clausula[2] < 0 and resp[-clausula[2] - 1] == 0 or clausula[2] >= 0 and resp[clausula[2] - 1] == 1:#prep 3
+        if clausula[2] < 0 and resp[(-clausula[2]) - 1] == 0 or clausula[2] >= 0 and resp[clausula[2] - 1] == 1:#prep 3
             p2 = 1
         else:
             p2 = 0         
@@ -53,7 +56,7 @@ def energia(prob, resp):
         #print("p0=", p0, " p1=", p1, " p2=", p2)
         if p0 == 1 or p1 == 1 or p2 == 1:#or
             e += 1
-    return (qtdC - e)*10
+    return (qtdC - e)
 
 def geraCandidato(c):
     nC = []
@@ -79,25 +82,28 @@ def sa():
         resposta += [random.randint(0, 1)]
 
     temperatura = t0
+    iteracoes = 0
     candidato = resposta
-    ener = energia(problema, candidato)
-
+    ener = energia(problema, candidato, qtdC)
+    print("Candidado inicial: ", candidato, " Energia inicial: ", ener)
+    
     #loop principal
-    while temperatura > tf and  ener != 0:
-        #print("Temperatura=", temperatura, "Energia=", 1/ener)
+    while temperatura > tf and  ener != 0 and iteracoes < maxIt:
         novoCandidato = geraCandidato(candidato)
-        nEner = energia(problema, novoCandidato)
+        nEner = energia(problema, novoCandidato, qtdC)
         delta = nEner - ener
-        print("Temperatura=", temperatura, "Energia=", ener)
         if delta <= 0:
             candidato = novoCandidato
             ener = nEner
         else:
-            print("chance=", pow(math.e, -delta/temperatura))
-            if random.random() < pow(math.e, -delta/temperatura):
+            #print("chance=", pow(math.e, -delta/temperatura))
+            if random.random() < math.exp(-delta / temperatura):
                 candidato = novoCandidato
                 ener = nEner
         temperatura = decTemperatura(temperatura)
+        iteracoes += 1
+        if(iteracoes % 100 == 0):
+            print("Temperatura= %.5f" % temperatura, "\tEnergia= ", ener, "\tIteracao= ", iteracoes)
         
     print("Resposta=", candidato, " | Energia=", ener, " | Minimo global=", 0)
 
